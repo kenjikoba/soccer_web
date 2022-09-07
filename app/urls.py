c761b1a191dbd3cafeaa0cda1c73c898f24778fb
kenjikoba <kenji.kobayashi0419@gmail.com>
2022-09-07 18:42:35 +0900

    lots of UI

--- app/urls.py
+++ app/urls.py
@@ -4,8 +4,6 @@ from . import views
 app_name = 'app'
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
-    path('app/user/', views.InputCreate, name='user_input'),
-    path('app/user/player', views.player_list, name='player_list'),
+    path('app/index/', views.index, name='index'),
     path('app/user/complete', views.move_to_output, name='user_input_complete'),
 ]
--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@
 # # def index(request):
 # #     return HttpResponse("Hello, world. You're at the app index.")
 
+# from attr import attributes
 from django.shortcuts import render
 
 from app.models import data, user_data
@@ -44,7 +45,7 @@ def move_to_output(request):
         # Weight = request.POST.get('Weight')
         # Age = request.POST.get('Age')
         # Position = request.POST.get('Position')
-        # Attributes= request.POST.get('Attributes')
+        user_attributes = request.POST.get('Attributes')
 
         # params = {
         #     "Height": Height,
@@ -59,7 +60,7 @@ def move_to_output(request):
         # Weight = request.GET.get('Weight')
         # Age = request.GET.get('Age')
         # Position = request.GET.get('Position')
-        # Attributes= request.GET.get('Attributes')
+        user_attributes = request.GET.get('Attributes')
 
         # params = {
         #     "Height": Height,
@@ -70,6 +71,12 @@ def move_to_output(request):
         # }
         user_position = request.GET.get('Position')
     # 入力をPositionだけDMとかなんかって入力したら検索できる。
-    player_results_list = list(data.objects.all().filter(position__contains=user_position).values_list())
+    # player_results_list = list(data.objects.all().filter(position__contains=user_position).values_list())
+    user_diff = user_height - int(user_data.objects.all().get(age__contains=user_age).male_height)
+    user_diff_min = user_diff - 5
+    user_diff_max = user_diff + 5
+    player_results_list = list(data.objects.all().filter(position__contains=user_position).filter(height_diff__gte=user_diff_min).filter(height_diff__lte=user_diff_max).filter(attributes__contains=user_attributes).values_list())
     result = {"result" : player_results_list} 
     return render(request, 'user_input_complete.html', result)
+
+    # .filter(attributes__contains=user_attributes)
